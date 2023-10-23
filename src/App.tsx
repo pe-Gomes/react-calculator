33e430010019f1f4979a6f2d1a776e364a7102fa
Pedro Gomes <pedrocgomes@proton.me>
2023-10-23 17:13:18 -0400

    feat: add calculator logic and components to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,137 @@
 import { Container, Content } from "./styles/styles";
 import { Input } from "./components/Input";
+import { Button } from "./components/Button";
+import { useState } from "react";
 
 function App() {
+  const [displayNumber, setDisplayNumber] = useState<string>("");
+  const [storedNumber, setStoredNumber] = useState<number | null>(null);
+  const [operation, setOperation] = useState<string | null>(null);
+
+  function handleAddNumber(value: number) {
+    setDisplayNumber((prevState) => `${prevState}${value}`);
+  }
+
+  function handleAddNumberDot() {
+    setDisplayNumber((prevState) => `${prevState}.`);
+  }
+
+  function handleOperationTypes(type: string) {
+    if (storedNumber === null) {
+      setStoredNumber(Number(displayNumber));
+      setDisplayNumber("");
+      setOperation(type);
+    }
+  }
+
+  function handleEquals() {
+    let result;
+
+    switch (operation) {
+      case "+":
+        result = storedNumber! + Number(displayNumber);
+        break;
+      case "—":
+        result = storedNumber! - Number(displayNumber);
+        break;
+      case "x":
+        result = storedNumber! * Number(displayNumber);
+        break;
+      case "%":
+        result = storedNumber! * (Number(displayNumber) / 100);
+        break;
+      case "÷":
+        result = storedNumber! / Number(displayNumber);
+    }
+    setDisplayNumber(result!.toString());
+    setOperation(null);
+  }
+
+  function handleCalculation(label: string | number) {
+    if (typeof label === "number") {
+      handleAddNumber(label);
+    } else {
+      switch (label) {
+        case "C":
+          setDisplayNumber("");
+          setStoredNumber(null);
+          break;
+        case "+":
+          handleOperationTypes("+");
+          break;
+        case "—":
+          handleOperationTypes("—");
+          break;
+        case "x":
+          handleOperationTypes("x");
+          break;
+        case "%":
+          handleOperationTypes("%");
+          break;
+        case "÷":
+          handleOperationTypes("÷");
+          break;
+        case ".":
+          handleAddNumberDot();
+          break;
+        case "=":
+          handleEquals();
+          break;
+      }
+    }
+  }
+
   return (
     <Container>
       <Content>
-        <Input />
+        <Input value={displayNumber} />
+        <div className="button-container">
+          <div className="row">
+            {["C", "%", "÷"].map((label) => (
+              <Button
+                key={label}
+                label={label}
+                onClick={() => handleCalculation(label)}
+              />
+            ))}
+          </div>
+          <div className="row">
+            {[7, 8, 9, "x"].map((label) => (
+              <Button
+                key={label}
+                label={label}
+                onClick={() => handleCalculation(label)}
+              />
+            ))}
+          </div>
+          <div className="row">
+            {[4, 5, 6, "—"].map((label) => (
+              <Button
+                key={label}
+                label={label}
+                onClick={() => handleCalculation(label)}
+              />
+            ))}
+          </div>
+          <div className="row">
+            {[1, 2, 3, "+"].map((label) => (
+              <Button
+                key={label}
+                label={label}
+                onClick={() => handleCalculation(label)}
+              />
+            ))}
+          </div>
+          <div className="row">
+            {[0, ".", "="].map((label) => (
+              <Button
+                key={label}
+                label={label}
+                onClick={() => handleCalculation(label)}
+              />
+            ))}
+          </div>
+        </div>
       </Content>
     </Container>
   );
--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -12,7 +12,31 @@ export const Container = styled.div`
 
 export const Content = styled.div`
   border-radius: 0 0 8px 8px;
-  background-color: #fff;
-  width: 80%;
-  min-height: 21rem;
+  background-color: #0c0c0c;
+  max-width: 80%;
+  min-width: 40%;
+
+  .row {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+
+  .row:first-child {
+    button {
+      background-color: #1c1c1c;
+    }
+  }
+
+  .row button:last-child {
+    background-color: #ff9f0a;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+
+  .row:first-child,
+  .row:last-child {
+    grid-template-columns: 2fr 1fr 1fr;
+  }
 `;
