d241587983d9c4c3361a82e21cb26ab16b91d1bb
Pedro Gomes <pedrocgomes@proton.me>
2023-10-23 11:06:41 -0400

    feat: add styles folder

---
+++ src/styles/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #0c0c0c;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Content = styled.div`
+  border-radius: 0 0 8px 8px;
+  background-color: #fff;
+  width: 80%;
+  min-height: 21rem;
+`;
